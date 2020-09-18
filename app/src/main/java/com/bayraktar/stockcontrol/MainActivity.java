13514405203f3c6412946698cd06fada181a4585
Emre Bayraktar <emrebyrktr54@gmail.com>
2020-09-18 21:27:57 +0300

    First commit
    Signed-off-by: Emre Bayraktar <emrebyrktr54@gmail.com>

---
+++ app/src/main/java/com/bayraktar/stockcontrol/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bayraktar.stockcontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
